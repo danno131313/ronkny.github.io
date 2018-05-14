b81f0dd5f8ba498bca79610233faf680b583f141
Daniel Kavanagh <dan.kava13@gmail.com>
2018-05-14 15:04:42 -0700

    fix

---
+++ src/js/base.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        autoplay: true,
+        autoplayTimeout: 6000,
+        loop: true,
+        dots: false,
+        smartSpeed: 700,
+        autoHeight: true,
+    });
+    
+    $('.ui.dropdown').dropdown({
+        on: 'hover'
+    });
+
+    $('#hamburger-menu-btn').click(function() {
+        $('.ui.sidebar').sidebar('toggle');
+    });
+});
